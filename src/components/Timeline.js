0804c252e0141cb5e6518de330a60387ea5446d4
Akramurridjal Rahman <xerod@outlook.com>
2020-04-23 11:39:06 +0700

    Timeline refinement

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,43 +8,66 @@ export default class BasicTimeLine extends Component {
     super();
     this.data = [
       {
-        time: "21 Dec",
+        time: "8:20 Today",
         title: "Event 1",
         description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        circleColor: "#e53e3e",
       },
       {
-        time: "12 Dec",
+        time: "20:00 Yesterday",
         title: "Event 2",
         description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
       },
       {
-        time: "19 Nov",
+        time: "17:21 Apr 20th",
         title: "Event 3",
         description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
+          "Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.",
       },
       {
-        time: "18 Nov",
+        time: "10:04 Apr 20th",
         title: "Event 4",
         description:
-          "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s.",
+          "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
       },
       {
-        time: "9 Oct",
+        time: "3:20 Apr 20th",
         title: "Event 5",
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+        description: "Lorem Ipsum was popularised in the 1960s.",
       },
     ];
   }
 
+  renderDetail(rowData) {
+    let time = (
+      <Text style={[tw._mT2, tw.textXs, tw.textGray700]}>{rowData.time}</Text>
+    );
+    var desc = null;
+    desc = (
+      <View style={[]}>
+        <Text>{rowData.description}</Text>
+      </View>
+    );
+
+    return (
+      <View style={{ flex: 1 }}>
+        {time}
+        {desc}
+      </View>
+    );
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Timeline style={{ flex: 1 }} data={this.data} />
+        <Timeline
+          style={[tw._mL6]}
+          circleColor={color.indigo600}
+          lineColor={color.indigo600}
+          data={this.data}
+          showTime={false}
+          renderDetail={this.renderDetail}
+        />
       </View>
     );
   }
