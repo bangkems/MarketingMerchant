dd97768d115616b66fc6e8b73b13b95afaa33607
Akramurridjal Rahman <xerod@outlook.com>
2020-04-28 03:46:27 +0700

    offer card refinement

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -7,35 +7,25 @@ export default class Offer extends Component {
   render() {
     return (
       <Card
-        style={[
-          tw.mX2,
-          tw.mY1,
-          tw.shadowMd,
-          tw.border2,
-          tw.borderPurple300,
-          tw.roundedLg,
-        ]}
+        style={[tw.mX2, tw.mY2, tw.border, tw.borderGray200, tw.rounded]}
         onPress={() =>
           NavigationService.navigate("OfferDetail", {
             tags: "",
           })
         }
       >
+        <Card.Title
+          title="Ripped Jeans Company"
+          subtitle="Bandung, Jawa Barat"
+        />
         <Card.Content>
-          <View style={[tw.mB4]}>
-            <Text style={[tw.textXl, tw.fontBold, tw.textGray800]}>
-              Ripped Jeans Company
-            </Text>
-          </View>
           <View style={[tw.flexRow, tw.justifyBetween, tw.itemsCenter]}>
             <View style={[tw.flexCol]}>
               <View style={[tw.flexRow, tw.itemsCenter, tw._mB2, tw._mL2]}>
-                <IconButton
-                  icon="speedometer"
-                  size={25}
-                  color={color.green700}
-                />
-                <Text style={[tw.textLg, tw._mL2, tw.textGreen700]}>89%</Text>
+                <IconButton icon="hanger" size={25} color={color.indigo400} />
+                <Text style={[tw.textLg, tw._mL2, tw.textIndigo400]}>
+                  Fashion
+                </Text>
               </View>
             </View>
             <View style={[tw.itemsEnd]}>
