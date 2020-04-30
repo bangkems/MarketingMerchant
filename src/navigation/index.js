963ef27fe0eb98af75bdfe48acce8fc6496373bc
Akramurridjal Rahman <xerod@outlook.com>
2020-04-30 21:08:24 +0700

    Remove screen validasi

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,13 +1,8 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "../screens";
-import HomeScreen from "../screens/home/index";
 import OfferScreen from "../screens/offer/index";
 import OfferDetail from "../screens/offer/detail";
-import Validasi from "../screens/Validasi/Validasi";
-import FormValidasi from "../screens/Validasi/FormValidasi";
-import KonfirmasiVoucher from "../screens/Validasi/KonfirmasiVoucher";
-import Sukses from "../screens/Validasi/Sukses";
 import Login from "../screens/OTP/Login";
 import Welcome from "../screens/OTP/Welcome";
 import OnboardingFans from "../screens/OnboardingFans";
@@ -22,18 +17,6 @@ const StackNavigator = createStackNavigator(
     },
     OfferDetail: {
       screen: OfferDetail,
-    },
-    Validasi: {
-      screen: Validasi,
-    },
-    FormValidasi: {
-      screen: FormValidasi,
-    },
-    KonfirmasiVoucher: {
-      screen: KonfirmasiVoucher,
-    },
-    Sukses: {
-      screen: Sukses,
     },
     Login: {
       screen: Login,
